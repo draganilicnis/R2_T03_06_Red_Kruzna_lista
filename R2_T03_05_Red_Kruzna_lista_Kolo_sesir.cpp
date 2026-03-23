39b50f32ed7859e581dfdada121c3f4add53fc8c
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2026-03-23 18:29:16 +0100

    Update R2_T03_05_Red_Kruzna_lista_Kolo_sesir.cpp

--- R2_T03_05_Red_Kruzna_lista_Kolo_sesir.cpp
+++ R2_T03_05_Red_Kruzna_lista_Kolo_sesir.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <list>
+#include <deque>
 #include <string>
 using namespace std;
+
 int main()
 {
     string s;
     cin >> s;                      // ULAZ: uuussuusis
-    list<int> a;                   // ekvivalent LinkedList<int>
+
+    deque<int> a;                   // ekvivalent LinkedList<int>
+
     for (int i = 0; i < s.length(); i++)
     {
         char c = s[i];
-        if (c == 'u') a.push_front(a.size() + 1); // dodavanje novog igraca na pocetak
-        else if (c == 'i' && !a.empty()) a.pop_front();// izbacivanje prvog
+
+        if (c == 'u')              // dodavanje novog igraca na pocetak
+            a.push_front(a.size() + 1);
+        else if (c == 'i' && !a.empty())   // izbacivanje prvog
+            a.pop_front();
         else if (c == 's' && !a.empty())   // rotacija (poslednji ide na pocetak)
         {
             int ispred_v = a.back();  // poslednji
@@ -19,16 +26,19 @@ int main()
             a.push_front(ispred_v);
         }
     }
+    // priprema za stampu
     if (!a.empty())
     {
         int v = a.front();
         a.pop_front();
         a.push_back(v);
     }
+    // IZLAZ: 1 2 4 5
     while (!a.empty())
     {
         cout << a.back() << " ";
         a.pop_back();
     }
+
     return 0;
 }
