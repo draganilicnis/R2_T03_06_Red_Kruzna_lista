c393e27c272af479ecc21d04f2d7f3e66e01494b
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2026-03-23 17:53:35 +0100

    Update R2_T03_05_Red_Kruzna_lista_LinkedList.cs

--- R2_T03_05_Red_Kruzna_lista_LinkedList.cs
+++ R2_T03_05_Red_Kruzna_lista_LinkedList.cs
@@ -1,47 +1,36 @@
 using System;
-using System.Collections.Generic;                       // Zbog LinkedList
+using System.Collections.Generic;               // Zbog LinkedList
 class R2_T03_05_Red_Kruzna_lista
 {
     static void Main()
     {
-        string s = Console.ReadLine();                  // ULAZ: uuussuusis
-        var a = new LinkedList<int>();                  // LinkedList
-        int b = 1;                                      // Broj (prvog slececeg) igraca u kolu: b = a.Count + 1;
+        string s = Console.ReadLine();          // ULAZ: uuussuusis
+        var a = new LinkedList<int>();          // LinkedList
         for (int i = 0; i < s.Length; i++)
         {
-            char c = s[i];
-            if (c == 'u')                               // Dodavanje novog igraca u kolo koji odmah postaje vodja
+            char c = s[i];  
+            if (c == 'u')                       // Dodavanje novog igraca: Nov igrac se dodaje na prvo mesto u kolo ispred vodje i postaje vodja 
+                a.AddFirst(a.Count + 1);                
+            else if (c == 'i' && a.Count > 0)   // Izbacivanje vodje iz kola (prvog elementa). Vodja je igracu iza starog (koji je bio na 2. poziciji)
+                a.RemoveFirst();                    
+            else if (c == 's' && a.Count > 0)   // Predavanje sesira narednom igracu (ispred sebe, odnosno poslednjem u kolu)
             {
-                a.AddFirst(b);                          // Nov igrac se dodaje na prvo mesto u kolo ispred onog koji ima sesir i uzima sesir od njega (postaje vodja i uzima sesir)
-                b++;                                    // Inkrementiramo ukupan broj igraca u kolu (za potencijalno sledeceg novog igraca)
-            }
-            else if (c == 's')                          // Predavanje sesira narednom igracu (ispred sebe, odnosno poslednjem u kolu)
-            {
-                if (a.Count > 0)
-                {
-                    int poslednji = a.Last.Value;       // Uzimamo vrednost poslednjeg igraca u kolu
-                    a.RemoveLast();                     // Izbacujemo poslednji element iz kola
-                    a.AddFirst(poslednji);              // Dodajemo na prvo mesto (kao vodju) igraca koji je bio poslednji u kolu (i uzima sesir)
-                }
-            }
-            else if (c == 'i')                          // Izbacicanje vodje iz kola
-            {
-                if (a.Count > 0)
-                {
-                    a.RemoveFirst();                    // Izbacujemo prvi element (vodju iz kola). Sesir predaje igracu iza sebe (koji je bio na 2. poziciji)
-                    b--;                                // Dekrementiramo ukupan broj igraca u kolu (za potencijalno sledeceg novog igraca)
-                }
+                int ispred_v = a.Last.Value;    // Uzimamo vrednost igraca koji je ispred vodje (posledenjeg u listi)
+                a.RemoveLast();                 // Izbacujemo poslednji element iz kola
+                a.AddFirst(ispred_v);           // Dodajemo na prvo mesto (kao vodju) igraca koji je bio poslednji u kolu (i uzima sesir)
             }
         }
 
-        int vodja = a.First.Value;                      // Uzimamo vrednost vodje (da bi ga prebacili na kraj liste)
-        a.RemoveFirst();                                // Izbacujemo vodju sa pocetka linkovane liste
-        a.AddLast(vodja);                               // Dodajemo vodju na kraj linkovane liste
-
-        while (a.Count > 0)                             // Sve dok ima elemenata u linkovanoj listi (igraca u kolu)
+        // Prirema za stampanje podataka
+        int v = a.First.Value;                  // Uzimamo vrednost vodje (da bi ga prebacili na kraj liste)
+        a.RemoveFirst();                        // Izbacujemo vodju sa pocetka linkovane liste
+        a.AddLast(v);                           // Dodajemo vodju na kraj linkovane liste
+        
+        // IZLAZ
+        while (a.Count > 0)                     // Sve dok ima elemenata u linkovanoj listi (igraca u kolu)
         {
-            Console.Write(a.Last.Value + " ");          // Stampamo vrednost poslednjeg elementa iz linkovane liste (FIFO) : 1 2 4 5
-            a.RemoveLast();                             // Izbacujemo poslednji element iz linkovane liste
+            Console.Write(a.Last.Value + " ");  // Stampamo vrednost poslednjeg elementa iz linkovane liste (FIFO) : 1 2 4 5
+            a.RemoveLast();                     // Izbacujemo poslednji element iz linkovane liste
         }
     }
 }
